9faf7867d4c8782b20dd2ce73d8cc22ebb5a9c64
Namrata Bilurkar <bilurkar.n@husky.neu.edu>
2018-05-01 03:22:33 -0400

    problem 442

--- all_duplicates_in_an_array.py
+++ all_duplicates_in_an_array.py
@@ -1,6 +1,28 @@
 '''
-Problem Number: 
-Difficulty level: 
-Link: https://leetcode.com/problems/
+Problem Number: 442
+Difficulty level: Medium
+Link: https://leetcode.com/problems/find-all-duplicates-in-an-array/description/
 Author: namratabilurkar
 '''
+
+'''
+Input:
+[4,3,2,7,8,2,3,1]
+
+Output:
+[2,3]
+'''
+
+from collections import Counter as c
+
+class Solution(object):
+    def findDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        return list(c(nums) - c(set(nums)))
+
+
+output = Solution()
+print(output.findDuplicates(nums=[4,3,2,7,8,2,3,1])) # [2,3]

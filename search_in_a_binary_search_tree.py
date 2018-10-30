577725b0070ba1bfecfc26a6442e0110164f6d45
Namrata Bilurkar <namratab94@users.noreply.github.com>
2018-10-29 22:07:38 -0400

    problem 700 -- recursive approach

---
+++ search_in_a_binary_search_tree.py
@@ -0,0 +1,32 @@
+'''
+Problem Number: 700
+Difficulty level: Easy
+Link: https://leetcode.com/problems/search-in-a-binary-search-tree/
+Author: namratabilurkar
+'''
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def searchBST(self, root, val):
+        """
+        :type root: TreeNode
+        :type val: int
+        :rtype: TreeNode
+        """
+        outputTree = []
+        if root == None:
+            return outputTree
+
+        if val == root.val:
+            return root
+        elif val < root.val:
+            return self.searchBST(root.left, val)
+        else:
+            return self.searchBST(root.right, val)

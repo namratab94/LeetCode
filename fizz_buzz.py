0b15a2d84002611fab053dbe8d4cc7141c926ce0
Namrata Bilurkar <namratab94@users.noreply.github.com>
2018-12-27 18:21:25 -0500

    problem 412

---
+++ fizz_buzz.py
@@ -0,0 +1,39 @@
+'''
+Problem Number: 412
+Difficulty level: Easy
+Link: https://leetcode.com/problems/fizz-buzz/description/
+Author: namratabilurkar
+'''
+
+'''
+Example:
+
+n = 15,
+
+Return:
+[
+    "1",
+    "2",
+    "Fizz",
+    "4",
+    "Buzz",
+    "Fizz",
+    "7",
+    "8",
+    "Fizz",
+    "Buzz",
+    "11",
+    "Fizz",
+    "13",
+    "14",
+    "FizzBuzz"
+]
+'''
+
+class Solution(object):
+    def fizzBuzz(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        return ['Fizz' * (not i % 3) + 'Buzz' * (not i % 5) or str(i) for i in range(1, n+1)]
